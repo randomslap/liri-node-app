cf486eb4492d04b88619108092f25fddddf09f4d
randomslap <kentokazaki@gmail.com>
2019-04-25 13:06:31 -0700

    early commit

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+var axios = require('axios');
+
+const command = process.argv[2]
+const arg = process.argv[3]
+
+switch (command) {
+    case 'concert-this':
+        
+}
